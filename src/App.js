241c47fb497074486a73de73c59656bd355499f3
alerner1 <alyssaplerner@gmail.com>
2020-12-20 18:53:04 -0500

    data and starter code is in

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { VictoryChart, VictoryAxis, VictoryBar, VictoryTheme } from 'victory';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <VictoryChart        domainPadding={20}   theme={VictoryTheme.material}>
+        <VictoryAxis
+          theme={VictoryTheme.material}
+          style={{tickLabels: {fontSize: 5, overflow: 'wrap', padding: 5}}}
+          
+        />
+        <VictoryAxis
+          dependentAxis
+          
+          theme={VictoryTheme.material}
+          style={{tickLabels: {fontSize: 5, overflow: 'wrap', padding: 5}}}
+          tickFormat={y => (`${y / 1000}k views`)}
+        />
+
+        <VictoryBar data={videoData} x="video" y="views" />
+      </VictoryChart>
     </div>
   );
 }
 
 export default App;
+
+const videoData =
+  [
+    {
+      video: "Blue Is Pretty Special: How Nature Gets the Blues",
+      views: 235678
+    },
+    {
+      video: "The Insect Nothing Messes With: Meet the Velvet Ant",
+      views: 502738
+    },
+    {
+      video: "Bacteria Could Someday Power Our Cell Phones",
+      views: 140846
+    },
+    {
+      video: "5 Ways Orcas Have Earned the Nickname 'Killer Whale'",
+      views: 192457
+    }
+  ];
